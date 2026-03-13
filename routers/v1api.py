4cd08059d1f203e9013578a07b912998845b468d
Hans <hi@hans0805.me>
2026-03-13 13:59:15 +0800

    feat: v1 -> v0

--- routers/v1api.py
+++ routers/v1api.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Response, Request
+from fastapi import APIRouter, Response, Request, status, Header, Depends
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 from dotenv import load_dotenv
@@ -7,16 +7,34 @@ import os
 import json
 import aiohttp
 import utils.v1 as v1
+from utils.http import HttpsClient
+import urllib.parse
 
 load_dotenv()
 router = APIRouter(prefix="/api/v1", tags=["v1 解析端點"])
+http = HttpsClient()
+
+
+class LoginCookie(BaseModel):
+    cookies: dict
+    school_name: str
 
 
 class HTMLInput(BaseModel):
     html: str
 
 
-@router.get("/", summary="獲取v1支援學校列表")
+def require_cookie_header(
+    cookie: str = Header(
+        ...,
+        alias="Cookie",
+        description="登入後取得的 Cookie 標頭",
+    )
+):
+    return cookie
+
+
+@router.get("/", summary="獲取此端點支援學校列表")
 async def index(request: Request):
     """
     獲取 v1 樣式解析支援的學校列表。
@@ -45,14 +63,41 @@ async def index(request: Request):
     return data
 
 
-@router.post("/merit_demerit", summary="解析獎懲紀錄")
-async def get_merit_demerit(request: Request, item: HTMLInput):
+@router.get("/merit_demerit", summary="解析獎懲紀錄")
+async def get_merit_demerit(
+    request: Request,
+    response: Response,
+    school_name: str,
+    _cookie: str = Depends(require_cookie_header),
+):
     """
-    解析獎懲紀錄的 HTML，回傳 JSON 格式的獎懲紀錄列表。
-     - **html**: 課程表的 HTML 字串。
-     - **返回值**: 包含獎懲紀錄列表的 JSON 物件。
+    取得獎懲紀錄的，回傳 JSON 格式的獎懲紀錄列表。
+     - 需帶入 cookies
+     - **返回值**: 包含學期成績資料的 JSON 物件。
+     
     """
-    r = v1.parse_merit_demerit_records(item.html)
+
+    school = request.app.state.schools.get(school_name)
+    data = request.app.state.response
+    if not school:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        data["code"] = 400
+        data["message"] = "Unsupported school."
+        data["data"] = None
+
+        return data
+
+    url = f"{school['api']}{school['route']['merit_demerit']}"
+    original_data = await http.get(url, request.cookies)
+    if not original_data.data:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        data["code"] = original_data.code
+        data["message"] = "Failed to fetch original data."
+        data["data"] = None
+
+        return data
+
+    r = v1.parse_merit_demerit_records(original_data.data)
 
     data = request.app.state.response
     data["code"] = 200
@@ -62,15 +107,41 @@ async def get_merit_demerit(request: Request, item: HTMLInput):
     return data
 
 
-@router.post("/curriculum", summary="解析課表")
-async def get_curriculum(request: Request, item: HTMLInput):
+@router.get("/curriculum", summary="解析課表")
+async def get_curriculum(
+    request: Request,
+    response: Response,
+    school_name: str,
+    _cookie: str = Depends(require_cookie_header),
+):
     """
-    解析課表的 HTML，回傳 JSON 格式的課程表資料。
-     - **html**: 課程表的 HTML 字串。
-     - **返回值**: 包含課程表資料的 JSON 物件。
-    
+    取得課表，回傳 JSON 格式的課程表資料。
+     - 需帶入 cookies
+     - **返回值**: 包含學期成績資料的 JSON 物件。
+
     """
-    r = v1.parse_weekly_curriculum(item.html)
+
+    school = request.app.state.schools.get(school_name)
+    data = request.app.state.response
+    if not school:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        data["code"] = 400
+        data["message"] = "Unsupported school."
+        data["data"] = None
+
+        return data
+
+    url = f"{school['api']}{school['route']['curriculum']}"
+    original_data = await http.get(url, request.cookies)
+    if not original_data.data:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        data["code"] = original_data.code
+        data["message"] = "Failed to fetch original data."
+        data["data"] = None
+
+        return data
+
+    r = v1.parse_weekly_curriculum(original_data.data)
     if r.get("error"):
         data = request.app.state.response
         data["code"] = 500
@@ -87,15 +158,41 @@ async def get_curriculum(request: Request, item: HTMLInput):
     return data
 
 
-@router.post("/attendance", summary="解析出缺紀錄")
-async def get_attendance(item: HTMLInput, request: Request):
+@router.get("/attendance", summary="解析出缺紀錄")
+async def get_attendance(
+    request: Request,
+    response: Response,
+    school_name: str,
+    _cookie: str = Depends(require_cookie_header),
+):
     """
-    解析出勤紀錄的 HTML，回傳 JSON 格式的出勤紀錄列表。
-     - **html**: 出勤紀錄的 HTML 字串。
-     - **返回值**: 包含出勤紀錄列表的 JSON 物件。
+    取得出勤紀錄，回傳 JSON 格式的出勤紀錄列表。
+     - 需帶入 cookies
+     - **返回值**: 包含學期成績資料的 JSON 物件。
     """
-    r = v1.parse_absence_records(item.html, filter_types=[])
-    
+
+    school = request.app.state.schools.get(school_name)
+    data = request.app.state.response
+    if not school:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        data["code"] = 400
+        data["message"] = "Unsupported school."
+        data["data"] = None
+
+        return data
+
+    url = f"{school['api']}{school['route']['attendance']}"
+    original_data = await http.get(url, request.cookies)
+    if not original_data.data:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        data["code"] = original_data.code
+        data["message"] = "Failed to fetch original data."
+        data["data"] = None
+
+        return data
+
+    r = v1.parse_absence_records(original_data.data, filter_types=[])
+
     data = request.app.state.response
     data["code"] = 200
     data["message"] = "Success."
@@ -104,15 +201,40 @@ async def get_attendance(item: HTMLInput, request: Request):
     return data
 
 
-@router.post("/exam_menu", summary="解析考試選單")
-async def get_exam_menu(item: HTMLInput, request: Request):
+@router.get("/exam_menu", summary="解析考試選單")
+async def get_exam_menu(
+    request: Request,
+    response: Response,
+    school_name: str,
+    _cookie: str = Depends(require_cookie_header),
+):
     """
-    解析考試選單的 HTML，回傳 JSON 格式的考試選單資料。
-     - **html**: 考試選單的 HTML 字串。
-     - **返回值**: 包含考試選單資料的 JSON 物件。
-     - **考試選單資料格式**: 包含以下欄位：
+    取得考試選單，回傳 JSON 格式的考試選單資料。
+     - 需帶入 cookies
+     - **返回值**: 包含學期成績資料的 JSON 物件。
     """
-    r = v1.parse_exam_menu(item.html)
+    school = request.app.state.schools.get(school_name)
+    data = request.app.state.response
+    if not school:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        data["code"] = 400
+        data["message"] = "Unsupported school."
+        data["data"] = None
+
+        return data
+
+    url = f"{school['api']}{school['route']['exam_menu']}"
+    original_data = await http.get(url, request.cookies)
+
+    if not original_data.data:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        data["code"] = original_data.code
+        data["message"] = "Failed to fetch original data."
+        data["data"] = None
+
+        return data
+
+    r = v1.parse_exam_menu(original_data.data)
 
     data = request.app.state.response
     data["code"] = 200
@@ -120,13 +242,14 @@ async def get_exam_menu(item: HTMLInput, request: Request):
     data["data"] = r
 
     return data
+
 
 @router.post("/exam_results", summary="解析考試成績")
-async def get_exam_results(item: HTMLInput, request: Request):
+async def get_exam_results(item: HTMLInput, request: Request, exam: str):
     """
-    解析考試成績的 HTML，回傳 JSON 格式的考試成績資料。
-     - **html**: 考試成績的 HTML 字串。
-     - **返回值**: 包含考試成績資料的 JSON 物件。
+    取得考試成績，回傳 JSON 格式的考試成績資料。
+     - 需帶入 cookies
+     - **返回值**: 包含學期成績資料的 JSON 物件。
     """
     r = v1.parse_exam_scores(item.html)
     if r.get("error"):
@@ -144,27 +267,66 @@ async def get_exam_results(item: HTMLInput, request: Request):
 
     return data
 
-@router.post("/semester_scores", summary="解析學期成績")
-async def get_semester_scores(item: HTMLInput, request: Request):
+
+@router.get("/semester_scores", summary="解析學期成績")
+async def get_semester_scores(
+    response: Response,
+    request: Request,
+    school_name: str,
+    semester: int = 1,
+    _cookie: str = Depends(require_cookie_header),
+):
     """
-    解析學期成績的 HTML，回傳 JSON 格式的學期成績資料。
-     - **html**: 學期成績的 HTML 字串。
+    取得學期成績，回傳 JSON 格式的學期成績資料。
+     - 需帶入 cookies
      - **返回值**: 包含學期成績資料的 JSON 物件。
     """
-    print(item.html)
-    r = v1.StudentGradeExtractor(item.html)
+    data = request.app.state.response
+
+    if semester < 1 or semester > 3:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        data["code"] = 400
+        data["message"] = "Invalid semester. Must be 1 ~ 3."
+        data["data"] = None
+
+        return data
+
+    school_info = request.app.state.schools.get(school_name)
+    if not school_info:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        data["code"] = 400
+        data["message"] = "Unsupported school."
+        data["data"] = None
+
+        return data
+
+    year_class = ["", "一", "二", "三"][semester]
+    url = f"{school_info['api']}{school_info['route']['semester_scores']}".replace(
+        "{year_class}", urllib.parse.quote(year_class.encode("big5"))
+    ).replace("{number}", str(semester))
+
+    original_data = await http.get(url, request.cookies)
+
+    if not original_data.data:
+        
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        data["code"] = 500
+        data["message"] = "Failed to fetch original data."
+        data["data"] = None
+
+        return data
+
+    r = v1.StudentGradeExtractor(original_data.data)
     r = r.get_all_grade_data()
-    
-    
+
     if r.get("error"):
-        data = request.app.state.response
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         data["code"] = 500
         data["message"] = r["error"]
         data["data"] = None
 
         return data
 
-    data = request.app.state.response
     data["code"] = 200
     data["message"] = "Success."
     data["data"] = r
