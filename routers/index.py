aba13c14a540640ac880cdfb6a1b1b458e59a0fe
Hans <hi@hans0805.me>
2026-03-15 22:07:54 +0800

    fix: remove unused import and update API endpoint parameter handling

--- routers/index.py
+++ routers/index.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter, Response, Request
 from fastapi.responses import RedirectResponse, FileResponse
+from dotenv import load_dotenv
 
-import utils.v1 as v1
+import os
 
 router = APIRouter()
-
-
-
+load_dotenv()
 
 @router.get("/", summary="首頁")
 async def index(request: Request):
@@ -17,7 +16,7 @@ async def get_all_schools(request: Request):
     return request.app.state.schools
 
 @router.get("/api/{v}", summary="獲取此端點支援學校列表")
-async def index(request: Request,v:str = v1):
+async def index(request: Request,v:str):
     """
     獲取 v2 樣式解析支援的學校列表。
      - **返回值**: 包含支援學校列表的 JSON 物件。
@@ -30,7 +29,8 @@ async def index(request: Request,v:str = v1):
     supported_schools = []
     for i in schools:
         if schools[i].get("vision") == str(v):
-            supported_schools.append(i)
+            if os.environ.geyt("ENV") == "production" and not schools[i].get("beta", False):
+               supported_schools.append(i)
     data["data"] = supported_schools
 
     return data
