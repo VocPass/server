d328a624c7489a63e006f7b74afd390e447dbe53
Hans <hi@hans0805.me>
2026-03-15 19:27:42 +0800

    feat: add parse_semester_grades function and improve merit/demerit record parsing

--- utils/v2.py
+++ utils/v2.py
@@ -1,5 +1,6 @@
 from utils.base import *
 
+
 def parse_curriculum(curriculum_data):
     """
     解析課表
@@ -29,6 +30,7 @@ def parse_curriculum(curriculum_data):
         )
     return data
 
+
 def parse_merit_demerit_records(records):
     """
     解析獎懲紀錄
@@ -36,23 +38,56 @@ def parse_merit_demerit_records(records):
     good = ["大功", "小功", "嘉獎"]
     bad = ["大過", "小過", "警告"]
 
-    data = [[],[]]
-    for record in records['obj']['ListD']:
+    data = [[], []]
+    for record in records["obj"]["ListD"]:
         i = 3
-        if record['RewardItem'][:2] in good:
+        if record["RewardItem"][:2] in good:
             i = 0
-        elif record['RewardItem'][:2] in bad:
+        elif record["RewardItem"][:2] in bad:
             i = 1
-        date = YearModel(record['RdDate'].replace("-", "/"))
+        date = YearModel(record["RdDate"].replace("-", "/"))
         data[i].append(
             {
-                "date_occurred": record['RdDate'].replace("-", "/"),
-                "date_approved": record['RdDate'].replace("-", "/") if record['ReformStatusText'] != "申請中" else "申請中",
-                "reason": record['Descript'],
-                "action": record['RewardItem'],
-                "date_revoked": None, # 佔位
+                "date_occurred": record["RdDate"].replace("-", "/"),
+                "date_approved": (
+                    record["RdDate"].replace("-", "/")
+                    if record["ReformStatusText"] != "申請中"
+                    else "申請中"
+                ),
+                "reason": record["Descript"],
+                "action": record["RewardItem"],
+                "date_revoked": None,  # 佔位
                 "year": f"{date.year}{date.semester}",
             }
         )
-        
+
     return data
+
+
+def parse_semester_grades(first_semester_grades, second_semester_grades):
+    data = {
+        "student_info": first_semester_grades["obj"]["StuName"],
+        "subject_scores": [],
+    }
+
+    for first, second in zip(
+        first_semester_grades["obj"]["DataList"],
+        second_semester_grades["obj"]["DataList"],
+    ):
+        d = {
+            "subject": first["OpTitle"],
+            "first_semester": {
+                "type": first["StudyType"],
+                "credits": str(first["Credit"]),
+                "score": "-",
+            },
+            "second_semester": {
+                "type": second["StudyType"],
+                "credits": str(second["Credit"]),
+                "score": "-",
+            },
+            "annual_score": "-",
+        }
+        data["subject_scores"].append(d)
+    return data
+        
