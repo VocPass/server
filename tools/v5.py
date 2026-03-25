4e61c2e89214afe30fabc171dc59d6ff1cbd570c
Hans <hi@hans0805.me>
2026-03-25 23:59:35 +0800

    fix: json

--- tools/v5.py
+++ tools/v5.py
@@ -102,40 +102,36 @@ def write_school_json():
         school = json.load(f)
 
     for i in new_schools:
-        if i in school:
-            continue
 
-        school[i] = (
-            {
-                "vision": "v6",
-                "app": 1.4,
-                "beta": False,
-                "api": "https://sschool.tp.edu.tw",
-                "url": {
-                    "login": f"/Login.action?schNo={new_schools[i]}",
-                    "logined": "/PersonalWidget_getMyWidget.action",
-                    "root": "/",
-                },
-                "login": {
-                    "username": {"name": "account"},
-                    "password": {"name": "password"},
-                    "captcha": {},
-                    "captchaImage": {},
-                    "button": {"class": "btn_submit"},
-                    "successKeywords": ["登出", ',"page":1,'],
-                },
-                "route": {
-                    "merit_demerit": "/B0305S_Reward_select.action?dataName=vo",
-                    "attendance": "/B0209S_Absence_select.action",
-                    "exam_menu": "/A0410S_Item_select.action?syear={syear}&seme={seme}",
-                    "exam_results": "#",
-                    "semester_scores": "/A0410S_OpenStdView_selectA0410s.action?pId={pId}",
-                    "curriculum": "/myLessons.action",
-                    "info": "/A0410S_StdSemeView_select.action?statusM={statusM}&stdId={stdId}",
-                    "login": "/LoginInfo.action",
-                },
+        school[i] = {
+            "vision": "v6",
+            "app": 1.4,
+            "beta": False,
+            "api": "https://sschool.tp.edu.tw",
+            "url": {
+                "login": f"/Login.action?schNo={new_schools[i]}",
+                "logined": "/PersonalWidget_getMyWidget.action",
+                "root": "/",
             },
-        )
+            "login": {
+                "username": {"name": "account"},
+                "password": {"name": "password"},
+                "captcha": {},
+                "captchaImage": {},
+                "button": {"class": "btn_submit"},
+                "successKeywords": ["登出", ',"page":1,'],
+            },
+            "route": {
+                "merit_demerit": "/B0305S_Reward_select.action?dataName=vo",
+                "attendance": "/B0209S_Absence_select.action",
+                "exam_menu": "/A0410S_Item_select.action?syear={syear}&seme={seme}",
+                "exam_results": "#",
+                "semester_scores": "/A0410S_OpenStdView_selectA0410s.action?pId={pId}",
+                "curriculum": "/myLessons.action",
+                "info": "/A0410S_StdSemeView_select.action?statusM={statusM}&stdId={stdId}",
+                "login": "/LoginInfo.action",
+            },
+        }
 
     with open("school.json", "w", encoding="utf-8") as f:
         json.dump(school, f, ensure_ascii=False, indent=4)
